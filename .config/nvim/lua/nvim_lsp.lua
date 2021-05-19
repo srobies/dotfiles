2e9541a7be9827563138959c2999982f2674b807
LrobieProject <sprobie1@gmail.com>
2021-05-19 03:05:05 -0500

    Cleanup lsp config

--- .config/nvim/lua/nvim_lsp.lua
+++ .config/nvim/lua/nvim_lsp.lua
@@ -1,14 +1,4 @@
 -- lsp-config setup
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-capabilities.textDocument.completion.completionItem.resolveSupport = {
-    properties = {
-        'documentation',
-        'detail',
-        'additionalTextEdits',
-    }
-}
-
 local nvim_lsp = require('lspconfig')
 local on_attach = function(client, bufnr)
 local wk = require("which-key")
@@ -18,7 +8,7 @@ local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
 local opts = { noremap=true, silent=true }
 buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
 buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+-- buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
 buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
 -- buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
 buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
@@ -77,16 +67,12 @@ for _, server in pairs(installed_servers) do
         require'lspconfig'[server].setup{}
     else -- special setup for clangd
         require'lspconfig'.cpp.setup{
-            vim.api.nvim_set_keymap('n', '<space>cs', ':ClangdSwitchSourceHeader<cr>', {noremap = true})
+            vim.api.nvim_set_keymap('n', '<space>cs', ':ClangdSwitchSourceHeader<cr>', {noremap = true}),
         }
     end
     nvim_lsp[server].setup { on_attach = on_attach }
 end
 
--- require'lspconfig'.cpp.setup{
---     capabilities = capabilities,
---     vim.api.nvim_set_keymap('n', '<space>h', ':ClangdSwitchSourceHeader<cr>', {noremap = true}),
--- }
 
 local saga = require 'lspsaga'
 saga.init_lsp_saga {
