5c11d6fb4642efdcf959f845efc157868273a190
srobies <sprobie1@gmail.com>
2021-12-13 07:29:30 -0600

    Updated wayland config qtile

--- .config/qtile/autostart_wayland_laptop.sh
+++ .config/qtile/autostart_wayland_laptop.sh
@@ -3,3 +3,5 @@
 /usr/lib/polkit-kde-authentication-agent-1 &
 /usr/lib/pam_kwallet_init &
 swapbg -i ~/Pictures/530895_souredapple_clouds.png &
+dropbox &
+easyeffects --gapplication-service &
--- .config/qtile/wayland_config.py
+++ .config/qtile/wayland_config.py
@@ -73,15 +73,15 @@ keys = [
 
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
-    # Key([mod, "control"], "h", lazy.layout.grow_left(),
-    #     desc="Grow window to the left"),
-    # Key([mod, "control"], "l", lazy.layout.grow_right(),
-    #     desc="Grow window to the right"),
-    # Key([mod, "control"], "j", lazy.layout.grow_down(),
-    #     desc="Grow window down"),
-    # Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
-    Key([mod, "control"], "k", lazy.layout.grow(), desc="Grow window"),
-    Key([mod, "control"], "k", lazy.layout.shrink(), desc="Shrink window"),
+    Key([mod, "control"], "h", lazy.layout.grow_left(),
+        desc="Grow window to the left"),
+    Key([mod, "control"], "l", lazy.layout.grow_right(),
+        desc="Grow window to the right"),
+    Key([mod, "control"], "j", lazy.layout.grow_down(),
+        desc="Grow window down"),
+    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    Key([mod], "g", lazy.layout.grow(), desc="Grow window"),
+    Key([mod], "s", lazy.layout.shrink(), desc="Shrink window"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
 
     # Toggle between split and unsplit sides of stack.
@@ -116,6 +116,19 @@ keys = [
     Key([], "XF86AudioMute", lazy.spawn("pactl set-sink-mute @DEFAULT_SINK@ toggle")),
     Key([], "XF86AudioRaiseVolume", lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ +5%")),
     Key([], "XF86AudioLowerVolume", lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ -5%")),
+
+    Key(["control", "mod1"], "F1", lazy.core.change_vt(1), desc="Go to virtual console 1"),
+    Key(["control", "mod1"], "F2", lazy.core.change_vt(2), desc="Go to virtual console 2"),
+    Key(["control", "mod1"], "F3", lazy.core.change_vt(3), desc="Go to virtual console 3"),
+    Key(["control", "mod1"], "F4", lazy.core.change_vt(4), desc="Go to virtual console 4"),
+    Key(["control", "mod1"], "F5", lazy.core.change_vt(5), desc="Go to virtual console 5"),
+    Key(["control", "mod1"], "F6", lazy.core.change_vt(6), desc="Go to virtual console 6"),
+    Key(["control", "mod1"], "F7", lazy.core.change_vt(7), desc="Go to virtual console 7"),
+    Key(["control", "mod1"], "F8", lazy.core.change_vt(8), desc="Go to virtual console 8"),
+    Key(["control", "mod1"], "F9", lazy.core.change_vt(9), desc="Go to virtual console 9"),
+    Key(["control", "mod1"], "F10", lazy.core.change_vt(10), desc="Go to virtual console 10"),
+    Key(["control", "mod1"], "F11", lazy.core.change_vt(11), desc="Go to virtual console 11"),
+    Key(["control", "mod1"], "F12", lazy.core.change_vt(12), desc="Go to virtual console 12"),
 ]
 
 groups = [Group(i) for i in "123456789"]
@@ -174,6 +187,7 @@ screens = [
                 widget.Battery(format='{percent:2.0%}'),
                 widget.BatteryIcon(),
                 widget.KeyboardLayout(configured_keyboards=['dvorak', 'us'], display_map={'us':'us', 'dvorak':'dv'}),
+                widget.StatusNotifier(),
                 widget.Clock(format='%Y-%m-%d %a %I:%M %p'),
                 widget.QuickExit(),
             ],
@@ -203,7 +217,7 @@ floating_layout = layout.Floating(float_rules=[
     Match(wm_class='makebranch'),  # gitk
     Match(wm_class='maketag'),  # gitk
     Match(wm_class='ssh-askpass'),  # ssh-askpass
-    Match(wm_class='spectacle'),  # ssh-askpass
+    Match(wm_class='ssh-askpass'),  # ssh-askpass
     Match(title='branchdialog'),  # gitk
     Match(title='pinentry'),  # GPG key password entry
 ])
