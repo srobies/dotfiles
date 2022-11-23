53ffd580a524ce02c89881611fd761a69c433a01
srobies <sprobie1@gmail.com>
2022-11-23 05:12:23 -0600

    nvim plugin updates

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,104 +1,103 @@
 return require('packer').startup(function()
+  use 'glepnir/lspsaga.nvim'
   use '~/repos/undotree-nvim'
   use 'unblevable/quick-scope'
   use 'jbyuki/one-small-step-for-vimkind'
-    use 'wbthomason/packer.nvim'
-    use 'kristijanhusak/orgmode.nvim'
-    use {'akinsho/org-bullets.nvim',
+  use 'wbthomason/packer.nvim'
+  use 'kristijanhusak/orgmode.nvim'
+  use {'akinsho/org-bullets.nvim',
+    config = function()
+      require('org-bullets').setup{}
+    end
+  }
+  use 'windwp/nvim-autopairs'
+  use 'christoomey/vim-tmux-navigator'
+  use 'mbbill/undotree'
+  use 'tpope/vim-fugitive'
+  use 'mfussenegger/nvim-dap'
+  use {
+      'rcarriga/nvim-dap-ui',
+      requires = {'mfussenegger/nvim-dap'},
       config = function()
-        require('org-bullets').setup{}
+          require('dapui').setup{}
       end
-    }
-    use 'windwp/nvim-autopairs'
-    use 'christoomey/vim-tmux-navigator'
-    use 'mbbill/undotree'
-    use 'tpope/vim-fugitive'
-    use 'mfussenegger/nvim-dap'
-    use {
-        'rcarriga/nvim-dap-ui',
-        requires = {'mfussenegger/nvim-dap'},
-        config = function()
-            require('dapui').setup{}
-        end
-    }
-    use 'folke/tokyonight.nvim'
-    use {
-        'kevinhwang91/nvim-bqf',
-    }
-    use {
-        'terrortylor/nvim-comment',
-        config = function ()
-            require('nvim_comment').setup {
-                comment_empty = false
-            }
-        end
-    }
-    use 'tpope/vim-repeat'
-    use {
-        'akinsho/nvim-toggleterm.lua',
-        config = function ()
-            require('toggleterm').setup{
-                open_mapping = [[<C-t>]]
-            }
-        end
-    }
-    use 'lervag/vimtex'
-    use {
-        'lewis6991/gitsigns.nvim',
-        requires = {'nvim-lua/plenary.nvim'},
-        event = 'VimEnter', -- Otherwise it produces an error when a :Git buffer is open.
-        config = function ()
-            require('gitsigns').setup{
-                numhl = true,
-            }
-        end
-    }
-    use 'ledger/vim-ledger'
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate',
-    }
-    use 'lukas-reineke/indent-blankline.nvim'
-    use {'windwp/windline.nvim',
-        requires = {'kyazdani42/nvim-web-devicons'}}
-    use {
-      "folke/trouble.nvim",
-      requires = "kyazdani42/nvim-web-devicons",
-      config = function()
-        require("trouble").setup {}
+  }
+  use 'folke/tokyonight.nvim'
+  use 'kevinhwang91/nvim-bqf'
+  use {
+      'terrortylor/nvim-comment',
+      config = function ()
+          require('nvim_comment').setup {
+              comment_empty = false
+          }
       end
-    }
-    use {
-      "folke/todo-comments.nvim",
-      requires = {{"nvim-lua/plenary.nvim"}, {'folke/trouble.nvim'}},
-      config = function()
-        require("todo-comments").setup {}
+  }
+  use 'tpope/vim-repeat'
+  use {
+      'akinsho/nvim-toggleterm.lua',
+      config = function ()
+          require('toggleterm').setup{
+              open_mapping = [[<C-t>]]
+          }
       end
-    }
-    use 'neovim/nvim-lspconfig'
-    use {
-        'hrsh7th/nvim-cmp',
-        requires = {
-            'hrsh7th/vim-vsnip',
-            'rafamadriz/friendly-snippets',
-            'ray-x/cmp-treesitter',
-            'hrsh7th/cmp-vsnip',
-            'hrsh7th/cmp-nvim-lua',
-            'hrsh7th/cmp-nvim-lsp',
-            'hrsh7th/cmp-path',
-            'hrsh7th/cmp-buffer',
-            'hrsh7th/cmp-omni',
-            'lukas-reineke/cmp-rg',
-            'hrsh7th/cmp-nvim-lsp-signature-help'
-        }
-    }
-    use {'nvim-telescope/telescope.nvim',
-        requires = {{'nvim-lua/plenary.nvim'},{'nvim-lua/popup.nvim'}}
-    }
-    use {
-      "folke/which-key.nvim",
-      config = function()
-        require("which-key").setup {}
+  }
+  use 'lervag/vimtex'
+  use {
+      'lewis6991/gitsigns.nvim',
+      requires = {'nvim-lua/plenary.nvim'},
+      event = 'VimEnter', -- Otherwise it produces an error when a :Git buffer is open.
+      config = function ()
+          require('gitsigns').setup{
+              numhl = true,
+          }
       end
-    }
+  }
+  use 'ledger/vim-ledger'
+  use {
+      'nvim-treesitter/nvim-treesitter',
+      run = ':TSUpdate',
+  }
+  use 'lukas-reineke/indent-blankline.nvim'
+  use {'windwp/windline.nvim',
+      requires = {'kyazdani42/nvim-web-devicons'}}
+  use {
+    "folke/trouble.nvim",
+    requires = "kyazdani42/nvim-web-devicons",
+    config = function()
+      require("trouble").setup {}
+    end
+  }
+  use {
+    "folke/todo-comments.nvim",
+    requires = {{"nvim-lua/plenary.nvim"}, {'folke/trouble.nvim'}},
+    config = function()
+      require("todo-comments").setup {}
+    end
+  }
+  use 'neovim/nvim-lspconfig'
+  use {
+      'hrsh7th/nvim-cmp',
+      requires = {
+          'hrsh7th/vim-vsnip',
+          'rafamadriz/friendly-snippets',
+          'ray-x/cmp-treesitter',
+          'hrsh7th/cmp-vsnip',
+          'hrsh7th/cmp-nvim-lua',
+          'hrsh7th/cmp-nvim-lsp',
+          'hrsh7th/cmp-path',
+          'hrsh7th/cmp-buffer',
+          'hrsh7th/cmp-omni',
+          'lukas-reineke/cmp-rg',
+      }
+  }
+  use 'ray-x/lsp_signature.nvim'
+  use {'nvim-telescope/telescope.nvim',
+      requires = {{'nvim-lua/plenary.nvim'},{'nvim-lua/popup.nvim'}}
+  }
+  use {
+    "folke/which-key.nvim",
+    config = function()
+      require("which-key").setup {}
+    end
+  }
 end)
--- .config/nvim/plugin/plugin-config.lua
+++ .config/nvim/plugin/plugin-config.lua
@@ -1,3 +1,28 @@
+require('lsp_signature').setup()
+
+vim.keymap.set("n", "]t", function()
+  require("todo-comments").jump_next()
+end, { desc = "Next todo comment" })
+
+vim.keymap.set("n", "[t", function()
+  require("todo-comments").jump_prev()
+end, { desc = "Previous todo comment" })
+
+-- lsp saga
+local keymap = vim.keymap.set
+local saga = require('lspsaga')
+saga.init_lsp_saga()
+keymap("n", "gf", "<cmd>Lspsaga lsp_finder<CR>", { silent = true })
+keymap({"n","v"}, "<leader>ca", "<cmd>Lspsaga code_action<CR>", { silent = true })
+keymap("n", "<leader>cr", "<cmd>Lspsaga rename<CR>", { silent = true })
+keymap("n", "gd", "<cmd>Lspsaga peek_definition<CR>", { silent = true })
+keymap("n", "<leader>cd", "<cmd>Lspsaga show_line_diagnostics<CR>", { silent = true })
+keymap("n", "<leader>cd", "<cmd>Lspsaga show_cursor_diagnostics<CR>", { silent = true })
+keymap("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", { silent = true })
+keymap("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>", { silent = true })
+keymap("n","<leader>o", "<cmd>LSoutlineToggle<CR>",{ silent = true })
+keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>", { silent = true })
+
 require("dapui").setup()
 -- small step for vimkind
 local dap = require"dap"
