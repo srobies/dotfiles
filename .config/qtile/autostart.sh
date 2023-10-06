06b26cebb656afd82845b9087cc03880ce64f706
srobies <sprobie1@gmail.com>
2023-10-06 16:19:50 -0600

    Edited qtile autostart

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 feh --bg-scale $HOME/Pictures/wallpapers/souredapple.png &
-dropbox &
+nextcloud &
 xrandr --output HDMI-0 --right-of DP-0 --mode 2560x1440 --rate 74.99 &
 xrandr --output DP-0 --mode 2560x1440 --rate 164.83 --primary &
 thunderbird &
--- .config/qtile/autostart_wayland.sh
+++ .config/qtile/autostart_wayland.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-/usr/lib/polkit-kde-authentication-agent-1 &
-/usr/lib/pam_kwallet_init &
 swapbg -i ~/Pictures/530895_souredapple_clouds.png &
-dropbox &
 easyeffects --gapplication-service &
+thunderbird &
+nextcloud &
+dunst &
+kanshi &
