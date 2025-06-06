33806a69b98f32bdde0033b4e4a81f33b04344df
srobies <sprobie1@gmail.com>
2025-06-06 17:50:53 -0600

    Update nvim orgmode config

--- .config/nvim/lua/plugin_config/orgmode.lua
+++ .config/nvim/lua/plugin_config/orgmode.lua
@@ -2,11 +2,10 @@ require'nvim-treesitter.configs'.setup {
   -- If TS highlights are not enabled at all, or disabled via `disable` prop, highlighting will fallback to default Vim syntax highlighting
   highlight = {
     enable = true,
-    -- disable = {'org'}, -- Remove this to use TS highlighter for some of the highlights (Experimental)
-    additional_vim_regex_highlighting = {'org'}, -- Required since TS highlighter doesn't support all syntax features (conceal)
   },
   
-  ensure_installed = {'org'}, -- Or run :TSUpdate org
+  ensure_installed = 'all', -- Or run :TSUpdate org
+  ignore_install = {'org'},
   incremental_selection = {
     enable = true,
     keymaps = {
