895d002bbea0d030d7e502221b536cc88ccc8ff2
srobies <sprobie1@gmail.com>
2025-07-12 14:27:31 -0600

    Remove all polybar, rofi, update niri, add waybar

--- .config/polybar/launch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-killall -q polybar
-killall xembedsniproxy
-while rg -u $UID -x polybar >/dev/null; do sleep 1; done
-
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar --reload mybar &
-done
